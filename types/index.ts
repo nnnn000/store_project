18aff5a9f5e33ca16ab781a97b9bd385be114a86
Narongton H. <Narongton_000@hotmail.com>
2026-02-16 00:06:18 +0700

    update-zod-validation

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,8 @@
-export type Product = {
+import { z } from "zod";
+import { insertProductSchema } from "@/lib/validators";
+
+export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
-  name: string;
+  rating: string;
+  createdAt: Date;
 };
